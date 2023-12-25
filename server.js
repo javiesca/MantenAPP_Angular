a7774b0480720fb85907b9af89bf92d5685d975e
Javier Viesca <viesca@gmail.com>
2023-12-25 12:44:47 +0100

    Cambios

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/proyecto-frontend'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/proyecto-frontend/vehiculos.html'));
+res.sendFile(path.join(__dirname+'/dist/proyecto-frontend/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
