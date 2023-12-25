8c47c94d7f7e7f2459d15b25be5aea31d28e5e57
javiesca <viesca@gmail.com>
2023-12-25 23:32:20 +0100

    Cambios

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -1,4 +1,4 @@
 export const environment  = {
   //apiBaseURL : 'https://manten-app-52e4cb231749.herokuapp.com/api/'
-  apiBaseURL : 'http://localhost/api/'
+  apiBaseURL : 'http://localhost:4200/api/'
 };
