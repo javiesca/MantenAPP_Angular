ddabb91366438328a9acdb9436d36cdf04e9c495
javiesca <viesca@gmail.com>
2023-12-25 23:31:33 +0100

    cambios

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -1,4 +1,4 @@
 export const environment  = {
   //apiBaseURL : 'https://manten-app-52e4cb231749.herokuapp.com/api/'
-  apiBaseURL : 'https://localhost:4200/api/'
+  apiBaseURL : 'http://localhost/api/'
 };
