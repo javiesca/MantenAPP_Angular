63326dbf2314da29b2c5403cf8fdbb00e0289b30
Javier Viesca <viesca@gmail.com>
2023-12-27 11:28:29 +0100

    GhPages

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -1,4 +1,4 @@
 export const environment  = {
   //apiBaseURL : 'https://manten-app-52e4cb231749.herokuapp.com/api/'
-  apiBaseURL : 'http://localhost:4200/api/'
+  apiBaseURL : 'http://13.49.246.12/api/'
 };
