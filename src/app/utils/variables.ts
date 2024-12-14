b1cecbee6460c03d99491e4d057f64a83be3afb1
javiesca <viesca@gmail.com>
2024-12-14 19:36:16 +0100

    Caracter suelto en pantalla de detalles

--- src/app/utils/variables.ts
+++ src/app/utils/variables.ts
@@ -1,5 +1,5 @@
 export const environment = {
   //apiBaseURL : 'https://manten-app-52e4cb231749.herokuapp.com/api/'
-  //apiBaseURL : 'https://mantenapp-spring.onrender.com/api/'
-  apiBaseURL: 'http://localhost:8080/api/'
+  apiBaseURL : 'https://mantenapp-spring.onrender.com/api/'
+  //apiBaseURL: 'http://localhost:8080/api/'
 };
