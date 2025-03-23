67cbbe39bf34cabb83aa1c43317f77f80ad1c703
javiesca <viesca@gmail.com>
2025-03-23 11:28:06 +0100

    Se agrega comentarios y variable para conectar al Beelink

--- src/app/utils/variables.ts
+++ src/app/utils/variables.ts
@@ -1,4 +1,11 @@
 export const environment = {
+  
+  //Render
   apiBaseURL : 'https://mantenapp-spring.onrender.com/'
+  
+  //Local
   //apiBaseURL: 'http://localhost:8080/'
+  
+  //Beelink
+  //apiBaseURL: 'http://192.168.0.50:8081/'
 };
