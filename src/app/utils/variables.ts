6ea14b18b08d240dfb29305c8293553d9782ace2
Javier Viesca <viesca@gmail.com>
2026-06-22 19:49:32 +0200

    Cambios

--- src/app/utils/variables.ts
+++ src/app/utils/variables.ts
@@ -1,11 +1,4 @@
 export const environment = {
-
-  //Render
-  apiBaseURL : 'https://mantenapp-spring.onrender.com/'
-
-  //Local
-  //apiBaseURL: 'http://localhost:8080/'
-
-  //Beelink
-  //apiBaseURL: 'http://192.168.0.50:8081/'
+  production: true,
+  apiBaseURL: '/api/'
 };
